d4602130f9d1ec7ede69fdb2ba1e30ee78144b06
analiabelo98 <analiabelo.42@gmail.com>
2021-04-22 13:20:02 +0000

    Card generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generateRandomNumber();
 };
+
+function generateRandomNumber() {
+  let array = ["8", "3", "Q", "K", "J", "A", "1", "2", "4", "5", "6", "7", "9"];
+  let numberIndex = Math.floor(Math.random() * array.length);
+
+  return array[numberIndex];
+}
+
+function generateRandomSuit() {
+  let suits = ["heart", "spade", "diamond", "club"];
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+
+  return suits[suitsIndex];
+}
